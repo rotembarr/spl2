3e9d6ce4a3df324190cec9a9cd64b93194803481
Rotem Bar <rotembar110@gmail.com>
2021-11-30 14:13:15 +0200

    initial commit

---
+++ src/test/java/bgu/spl/mics/MessageBusTest.java
@@ -0,0 +1,13 @@
+package bgu.spl.mics;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MessageBusTest {
+	@Test
+	public void test1 () {
+		System.out.println("heyooo");
+		assertEquals(1, 2);
+	}
+}
